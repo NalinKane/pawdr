461895d7e3c244a2599c832533f77541d60cf58c
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-03-15 13:31:33 +0000

    Add DB seed

---
+++ seeders/users.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+
+const seeder = require("mongoose-seed");
+const db = require("../config/keys").mongoURI;
+const bcrypt = require("bcryptjs");
+
+async function hashPasswords(data) {
+  // Create list of promises per each user
+  const promises = data.map(function(user) {
+    return new Promise((resolve, reject) => {
+      bcrypt.hash(user.password, 10, (err, hash) => {
+        if (err) throw err;
+        user.password = hash;
+        resolve(user);
+      });
+    });
+  });
+
+  return Promise.all(promises);
+}
+
+seeder.connect(db, async () => {
+  seeder.loadModels(["models/User.js"]);
+
+  const users = await hashPasswords(data[0].documents);
+  data[0].documents = users;
+
+  seeder.clearModels(["users"], function() {
+    seeder.populateModels(data, function() {
+      seeder.disconnect();
+    });
+  });
+});
+
+const data = [
+  {
+    model: "users",
+    documents: [
+      {
+        firstName: "Shaggy",
+        lastName: "Scragmore",
+        location: "Shrewsbury",
+        username: "shagger",
+        email: "shagmore@gmail.com",
+        password: "123123"
+      },
+      {
+        firstName: "Sean",
+        lastName: "Bean",
+        location: "Shrewsbury",
+        username: "beanz",
+        email: "sbean@gmail.com",
+        password: "qwerty"
+      },
+      {
+        firstName: "Lily",
+        lastName: "Belle",
+        location: "Shrewsbury",
+        username: "lbell",
+        email: "lilz@aol.com",
+        password: "password"
+      },
+      {
+        firstName: "Rufus",
+        lastName: "Dufus",
+        location: "Shrewsbury",
+        username: "Drufus",
+        email: "druffy@gmail.com",
+        password: "lame123"
+      },
+      {
+        firstName: "Sin",
+        lastName: "Tzu",
+        location: "Shrewsbury",
+        username: "Stint",
+        email: "sintzu@gmail.com",
+        password: "oynoy"
+      },
+      {
+        firstName: "Kelly",
+        lastName: "Craig",
+        location: "Shrewsbury",
+        username: "Kellz",
+        email: "lula@gmail.com",
+        password: "nomnom"
+      }
+    ]
+  }
+];
