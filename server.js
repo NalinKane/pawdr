d4b36c2e1bcb16ef7d349d790bf2f5075ccd711f
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-03-20 22:53:45 +0000

    add missing path to server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
