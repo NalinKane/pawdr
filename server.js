ba7670e59da43640aa62345fc48a5f77cc8e764b
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-03-20 22:46:49 +0000

    add server support for react router

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("client/build", "index.html"))
+  );
 }
 
 app.listen(PORT, function() {
